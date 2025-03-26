b7cb17ea0bc900e3d2b834bbe7370bdafd58194b
Aryan <arynch24@gmail.com>
2025-03-26 11:18:29 +0530

    Add "use client" to page.js and NFTBids.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
-import NFTBids from "@/components/NFTBids";
+"use client"
 
+import NFTBids from "@/components/NFTBids";
 export default function Home() {
     return (
         <main className="min-h-screen flex items-center justify-center bg-gray-950 text-white">
