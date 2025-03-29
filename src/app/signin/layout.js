df3e86ab7c0f7cb705b76b13e9e346e10d58cc7f
Aryan <arynch24@gmail.com>
2025-03-29 15:12:40 +0530

    Remove unused code

--- src/app/signin/layout.js
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }) {
-    return (
-      <div className="flex items-center justify-center min-h-screen text-white">
-        {children} {/* Only show sign-in content */}
-      </div>
-    );
-  }  
